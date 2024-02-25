f381df4477a4256049bd9386ae37b4c2479ca1e4
kunley247 <cafeat9ja@gmail.com>
2024-02-25 13:44:33 +0000

    update

--- laravel/app/Http/Controllers/API/LevelController.php
+++ laravel/app/Http/Controllers/API/LevelController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use App\Models\Levels;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
 
 class LevelController extends Controller
 {
